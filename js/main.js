b91f8a66d35ea644a0fef04ce0656aae23cce0bb
Asta Havshoj <astah2000@gmail.com>
2025-01-03 15:30:28 +0100

    mere indhold

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
+// const myImage = document.querySelector("#myImage");
+
+// myImage.addEventListener("click", () => {
+//   const mySrc = myImage.getAttribute("src");
+//   if (mySrc === "billeder/statemachine.webp") {
+//     myImage.setAttribute("src", "billeder/aktivitetsdiagram.webp");
+//   } else {
+//     myImage.setAttribute("src", "billeder/statemachine.webp");
+//   }
+// });
+
 // Find billedet med ID'et myImage
 const myImage = document.querySelector("#myImage");
 
 // Tilføj klik-event, der skifter billedet
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src"); // Henter den aktuelle src
+
+  // Skift mellem tre forskellige billeder i den ønskede rækkefølge
   if (mySrc === "billeder/statemachine.webp") {
     myImage.setAttribute("src", "billeder/aktivitetsdiagram.webp");
-  } else {
+  } else if (mySrc === "billeder/aktivitetsdiagram.webp") {
+    myImage.setAttribute("src", "billeder/kode_spil.webp");
+  } else if (mySrc === "billeder/kode_spil.webp") {
     myImage.setAttribute("src", "billeder/statemachine.webp");
   }
 });
